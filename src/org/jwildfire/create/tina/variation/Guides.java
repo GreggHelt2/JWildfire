13e9c0dce16296f2556837651a58d687476e6055
Gregg Helt <gregghelt@gmail.com>
2017-03-14 19:44:19 -0700

    Added Guides interface that can be implemented by variations funcs to draw guidelines on one of the render threads.

---
+++ src/org/jwildfire/create/tina/variation/Guides.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jwildfire.create.tina.variation;
+
+/**
+ *
+ * @author gregg
+ */
+public interface Guides {
+  public void setDrawGuides(boolean draw);
+  public boolean getDrawGuides();
+}
